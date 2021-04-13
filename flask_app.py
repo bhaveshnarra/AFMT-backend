5ac2732689dd4cb783ab8c8fe412b3ac8878d0c4
Siddhant Singh <siddhant9868@gmail.com>
2021-04-13 11:52:12 +0530

    port80

--- flask_app.py
+++ flask_app.py
@@ -32,4 +32,4 @@ def downloadFile ():
     return flask.send_file(path, as_attachment=True, cache_timeout=0)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
